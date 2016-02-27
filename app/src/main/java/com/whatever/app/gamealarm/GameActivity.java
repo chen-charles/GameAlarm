393d93e3b8ec557a1d1cb71e3c8f0dc2e6da9a16
Leon Ouyang <leonouyang417@gmail.com>
2016-02-27 17:24:26 -0500

    Added Game Activity

---
+++ app/src/main/java/com/whatever/app/gamealarm/GameActivity.java
@@ -0,0 +1,83 @@
+package com.whatever.app.gamealarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        Button ans1 = (Button) findViewById(R.id.ans1);
+        Button ans2 = (Button) findViewById(R.id.ans2);
+
+        int dif = new Random().nextInt(5)+1;
+        int val1 = new Random().nextInt(50)+1;
+        int num1a = new Random().nextInt(50)+1;
+        int num2a = new Random().nextInt(50)+1;
+        int val2, num1b, num2b;
+
+        if (new Random().nextDouble()<0.5){
+            val2 = val1+dif;
+            num1b = val1-num1a;
+            if (num1b<0){
+                ans1.setText(""+num1a+" + ("+num1b+")");
+            }
+            else{
+                ans1.setText(""+num1a+" + "+num1b);
+            }
+        }
+        else{
+            val2 = val1-dif;
+            num1b = val1 + num1a;
+            if (num1b<0){
+                ans1.setText(""+num1a+" - ("+num1b+")");
+            }
+            else{
+                ans1.setText(""+num1a+" - "+num1b);
+            }
+        }
+
+        if (new Random().nextDouble()<0.5){
+            num2b = val2-num2a;
+            if (num2b<0){
+                ans2.setText(""+num2a+" + ("+num2b+")");
+            }
+            else{
+                ans2.setText(""+num2a+" + "+num2b);
+            }
+        }
+        else{
+            num2b = val2 + num2a;
+            if (num2b<0){
+                ans2.setText(""+num2a+" - ("+num2b+")");
+            }
+            else {
+                ans1.setText(""+num2a+" - "+num2b);
+            }
+        }
+
+
+
+        ans1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ans2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/whatever/app/gamealarm/MainActivity.java
+++ app/src/main/java/com/whatever/app/gamealarm/MainActivity.java
@@ -1,7 +1,9 @@
 package com.whatever.app.gamealarm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button setbutton = (Button) findViewById(R.id.button);
+        Button dismissbutton = (Button) findViewById(R.id.button2);
+        Button cancelbutton = (Button) findViewById(R.id.button3);
+
+
+        Intent intt = new Intent(this, GameActivity.class );
+        startActivity(intt);
     }
 }
